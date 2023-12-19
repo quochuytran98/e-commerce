5dad0cfa2073855f337cedd14e756294d3266675
Trần Quốc Huy <huytq3@payme.vn>
2023-12-19 13:52:39 +0700

    update mongo uri

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://tranquochuy.id.vn/ticket'),
+    MongooseModule.forRoot('mongodb+srv://ltht1999:quochuy98@cluster1.dz34val.mongodb.net/ticket'),
     ConfigModule.forRoot() // Load environment variables
   ]
 })
